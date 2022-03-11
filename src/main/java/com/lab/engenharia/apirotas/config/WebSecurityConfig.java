9a939ad8724dfada860b729d95877d7ede6a9502
Davi Seagull <davi.seagull@gmail.com>
2022-03-11 10:18:58 -0300

    chore: adding swagger to whitelist of auth

---
+++ src/main/java/com/lab/engenharia/apirotas/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.lab.engenharia.apirotas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/v3/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**").permitAll()
+                .antMatchers("/**").authenticated()
+                .and()
+                .httpBasic();
+    }
+}
