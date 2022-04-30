fc0c4db7fffdcdf3d8588cc86e785cc686d8ccfe
Davi Seagull <davi.seagull@gmail.com>
2022-04-30 12:46:23 -0300

    fix: cors

--- src/main/java/com/lab/engenharia/apirotas/config/WebSecurityConfig.java
+++ src/main/java/com/lab/engenharia/apirotas/config/WebSecurityConfig.java
@@ -10,16 +10,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/v3/api-docs",
+        http.cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/v3/api-docs",
                         "/configuration/ui",
                         "/swagger-resources/**",
                         "/configuration/security",
                         "/swagger-ui.html",
                         "/webjars/**",
                         "/v3/api-docs/**",
-                        "/swagger-ui/**").permitAll()
-                .antMatchers("/**").authenticated()
+                        "/swagger-ui/**")
+                .permitAll()
+                .antMatchers("/**")
+                .authenticated()
                 .and()
                 .httpBasic();
     }
